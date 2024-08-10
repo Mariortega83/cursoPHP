a0633d599335daaf73799b3f007cb06a9ebc860d
Mario Ortega <mortper0408@ieszaidinvergeles.org>
2024-08-10 18:34:04 +0200

    primera parte de operadores

--- expresiones_operadores.php
+++ expresiones_operadores.php
@@ -26,8 +26,78 @@ $ingresos = $sueldo + $subsidioTransporte;
 $gastos = $salud + $pension;
 $total = $ingresos - $gastos;
 
-echo "El sueldo total es: $total"
+echo "El sueldo total es: $total";
 
 
+// Calcular el area de un cuadrado
+
+$base = 10 ;
+$altura = 14 ; 
+
+$area = $base * $altura;
+
+echo "<br>";
+echo $area;
+
+// Division
+
+$num1 = 8 ;
+$num2 = 2 ; 
+
+$division = $num1 / $num2;
+
+echo "<br>";
+echo $division;
+
+// Porcentaje
+
+$sumaNotas = 42 ;
+$asignaturas = 5 ; 
+
+$media = $sumaNotas / $asignaturas;
+
+echo "<br>";
+echo $media;
+
+// Modulo
+echo "<br>";
+$a = 51 % 2;
+echo $a;
+
+
+// Exponenciacion
+
+echo "<br>";
+$c = 4;
+$d = 3;
+$resualtado = $c ** $d;
+echo $resualtado;
+
+// Operadores condicionales
+
+$puedeIngresar = (true) ?//Pregunta, como un if
+ "ENTRA" // Si es verdad
+: "NO ENTRA" // Si es falso
+; // ? true : false
+
+echo "<br>";
+echo $puedeIngresar;
+
+
+// Operadores logicos and or %% ||
+
+$permiso = true;
+$autenticado = true;
+echo "<br>";
+echo ($permiso or $autenticado) ? "Bienvendio" : "No bienvenido";
+echo "<br>";
+echo ($permiso and $autenticado) ? "Bienvendio" : "No bienvenido";
+echo "<br>";
+
+$total = 50;
+$pasaMateria = 50;
+$validar = ($total>=$pasaMateria) ? true : false;
+
+echo ($validar) ? "Pasa" : "No pasa"
 
 ?>
